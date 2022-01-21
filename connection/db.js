41c15b14a0c417bc3dc663fef016a0f122c5091c
afif makkruf <muhafifmaruf@gmail.com>
2022-01-22 01:43:32 +0700

    make change

--- connection/db.js
+++ connection/db.js
@@ -2,11 +2,10 @@
 const { Pool }  = require('pg')
 
 const dbPool = new Pool({
-    host: 'ec2-34-194-171-47.compute-1.amazonaws.com',
-    database: 'dci6qdenpdees',
-    port: 5432,
-    user: 'ltjikctdsuzngl',
-    password: '316270c46964998b3a12cbb71cd3f9367c67cfc4b2e46bdf430afdfca9992003',
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false
+    }
 }) 
 
 
